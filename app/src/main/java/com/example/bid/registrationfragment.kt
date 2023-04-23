55574ded3504bef0338fcab66649536a31b1386e
Aman-Usman123 <120277244+Aman-Usman123@users.noreply.github.com>
2023-04-23 19:37:01 +0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/bid/registrationfragment.kt
+++ app/src/main/java/com/example/bid/registrationfragment.kt
@@ -2,6 +2,7 @@ package com.example.bid
 
 import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
+import android.net.Uri
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Email
 import android.util.Log
@@ -9,22 +10,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.actionCodeSettings
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 
 class registrationfragment : Fragment() {
 private lateinit var firebaseAuth: FirebaseAuth
-
-
-
+    private lateinit var uri: Uri
+    private var storageReference= Firebase.storage
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
@@ -36,14 +38,28 @@ private lateinit var firebaseAuth: FirebaseAuth
         // Inflate the layout for this fragment
         val v= inflater.inflate(R.layout.fragment_registrationfragment, container, false)
 
-        firebaseAuth=FirebaseAuth.getInstance()
+
+                    firebaseAuth=FirebaseAuth.getInstance()
 val btn =v.findViewById<Button>(R.id.registerbtn)
 val login=v.findViewById<TextView>(R.id.logtext)
+        storageReference= FirebaseStorage.getInstance()
+        val imageviewq=v.findViewById<ImageView>(R.id.imageView23)
+        val uplodcnic=v.findViewById<TextView>(R.id.uplod)
+        val imageGallery=registerForActivityResult(
+
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback{ imagePath ->
+                imageviewq.setImageURI(imagePath)
+                uri= imagePath!!;
+            })
+        imageviewq.setOnClickListener {
+            imageGallery.launch("image/*")
+        }
         btn.setOnClickListener {
 
-            val firstname = v.findViewById<EditText>(R.id.firstname).text.toString()
-            val lastname = v.findViewById<EditText>(R.id.lastname).text.toString()
-            val phones = v.findViewById<EditText>(R.id.phonenumber).text.toString()
+            val firstname = v.findViewById<EditText>(R.id.firstname)
+            val lastname = v.findViewById<EditText>(R.id.lastname)
+            val phones = v.findViewById<EditText>(R.id.phonenumber)
             val passwords = v.findViewById<EditText>(R.id.password).text.toString()
             val Emails = v.findViewById<EditText>(R.id.emailf).text.toString()
 if(Emails.isNotEmpty()&& passwords.isNotEmpty())
@@ -51,17 +67,38 @@ if(Emails.isNotEmpty()&& passwords.isNotEmpty())
 firebaseAuth.createUserWithEmailAndPassword(Emails,passwords).addOnCompleteListener {
 
     if(it.isSuccessful)
-    {val currentUser = FirebaseAuth.getInstance().currentUser
+
+    {
+        val currentUser = FirebaseAuth.getInstance().currentUser
         currentUser?.sendEmailVerification()
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    // Email sent successfully
+                    storageReference.getReference("Images").child(System.currentTimeMillis().toString())
+                        .putFile(uri)
+
+                        .addOnSuccessListener {task ->
+                            task.metadata!!.reference!!.downloadUrl.addOnSuccessListener { downlaodUrl ->
+                                // Email sent successfully
+                                val uid = FirebaseAuth.getInstance().currentUser!!.uid
+
+                                val imageMap1 = mapOf(
+                                    "username" to firstname.text.toString(),
+                                    "cnicnumber" to lastname.text.toString(),
+                                    "phnumber" to phones.text.toString(),
+                                    "cnicurl" to downlaodUrl.toString()
+                                    )
+                                val databaseReference =
+                                    FirebaseDatabase.getInstance().getReference("UserImagesData")
+                                databaseReference.child(uid).child("UserInfo").child("UserData")
+                                    .setValue(imageMap1)
+
+                            }}
+
                     Toast.makeText(requireContext(),"Go to Your Email Please",Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(requireContext(),"Sorry Try again",Toast.LENGTH_SHORT).show()
                 }
             }
-
 
     }else
     {
--- app/src/main/java/com/example/bid/usserImages.kt
+++ app/src/main/java/com/example/bid/usserImages.kt
@@ -2,7 +2,12 @@ package com.example.bid
 
 data class usserImages(
     var ImageUrl:String?=null,
-    var product:String?=null, var catagory:String?=null,
-    var weight:String?=null, var sizes:String?=null
-
+    var product:String?=null,
+    var catagory:String?=null,
+    var weight:String?=null,
+    var sizes:String?=null,
+    var username:String?=null,
+    var cnicnumber:String?=null,
+    var phnumber:String?=null,
+    var cnicurl:String?=null
 )
